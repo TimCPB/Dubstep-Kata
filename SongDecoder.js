e4d3333affcc9ec1d00d69baa28e2229d7f8ad38
Tim <TimCPB@github.com>
2020-09-05 00:25:28 +0100

    songDecoder returns array

--- SongDecoder.js
+++ SongDecoder.js
@@ -1,8 +1,9 @@
 function songDecoder(song){
   array = song.split('')
-  while(array[0]) === 'W' && array[1] === 'U' && array[2] === 'B'){
+  while(array[0] === 'W' && array[1] === 'U' && array[2] === 'B'){
     array.splice((array.length -3), 3)
     array.splice(0,3)
   }
-  newSong = array.join()
+  return array
+  });
 }
