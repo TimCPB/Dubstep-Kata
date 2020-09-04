c81e5eff7599a9cf383f389f72717f88d31e15b7
Tim <TimCPB@github.com>
2020-09-05 00:14:52 +0100

    modify SongDecoder.js

---
+++ SongDecoder.js
@@ -0,0 +1,8 @@
+function songDecoder(song){
+  array = song.split('')
+  while(array[0]) === 'W' && array[1] === 'U' && array[2] === 'B'){
+    array.splice((array.length -3), 3)
+    array.splice(0,3)
+  }
+  newSong = array.join()
+}
